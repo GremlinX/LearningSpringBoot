edefa2c1247b2d003c6e97283ea4a542f6b9a242
Marcelo Ito <marcelo-ito@hotmail.com>
2023-07-30 18:59:02 -0300

    Aula 313 - 30/07/2023

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -3,6 +3,8 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +28,7 @@ public class Payment implements Serializable {
 	 *  Which means that this class needs to be given the annotation "@OneToOne"
 	 *  and "@MapsId".
 	 */
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
